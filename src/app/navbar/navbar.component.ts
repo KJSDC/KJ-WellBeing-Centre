5a55f388dfcc331e78dee9512207399348698b8f
Kristu Jayanti Software Development Centre <128048963+KJC-SDC@users.noreply.github.com>
2025-08-13 17:40:23 +0530

    Update navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -246,7 +246,7 @@ Verify() {
   });
 }
 changePassword() {
-  console.log(this.ForgotPass.value);
+  console.log(this.ForgotPass);
   const password = this.ForgotPass.get('Password')?.value ?? '';
   const ConfirmPassword = this.ForgotPass.get('ConfirmPassword')?.value ?? '';
    console.log(password)
